0301a98bc842661ab58811533e624032f0786406
zhaoml <zml_xrzs@163.com>
2017-12-11 17:02:43 +0800

    Initial Commit

---
+++ animate/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  animate
+//
+//  Created by zhaoml on 2017/12/11.
+//  Copyright © 2017年 赵明亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ animate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  animate
+//
+//  Created by zhaoml on 2017/12/11.
+//  Copyright © 2017年 赵明亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
